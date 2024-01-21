653ad017300611020a44354f7cfe943e821ecd70
Jacek Kamiński <jacus502@gmail.com>
2024-01-21 21:12:54 +0100

    create css account

--- src/views/account.php
+++ src/views/account.php
@@ -1,43 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../../public/css/global.css">
+    <title>Account</title>
     <link rel="stylesheet" href="../../public/css/account.css">
+    <link rel="stylesheet" href="../../public/css/global.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700;900&display=swap"
+          rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-<section class="avatar-form">
-    <h1>UPLOAD</h1>
-    <form action="addImage" method="POST" ENCTYPE="multipart/form-data">
-        <div class="messages">
-            <?php
-            if(isset($messages)){
-                foreach($messages as $message) {
-                    echo $message;
-                }
-            }
-            ?>
+<div class="base-container">
+    <nav>
+        <div class="logo_container">
+            <img class="logo" src="../../public/assets/logo.png" alt="Logo">
+            <div class="textLogo">SELL-IT</div>
         </div>
-        <input name="title" type="text" placeholder="title">
-        <textarea name="description" rows=5 placeholder="description"></textarea>
+        <div class="Options">
+            <a class="textHome" href="/dashboard">HOME</a>
+            <a class="textCreate" href="/create">CREATE</a>
+            <a class="textAccount" href="/account">ACCOUNT</a>
+        </div>
+        <img class="avatarImage" src="../../public/assets/profile_empty.png" alt="avatarImage">
+    </nav>
+    <div class="content">
+        <div class="Avatar"></div>
 
-        <input type="file" name="file"/><br/>
-        <button type="submit">send</button>
-    </form>
-    <section class="avatars">
-        <div id="avatar-1">
-            <img src="public/uploads/<?= $avatar->getImage() ?>">
-            <div>
-                <h2><?= $avatar->getTitle() ?></h2>
-                <p><?= $avatar->getDescription() ?></p>
-                <div class="social-section">
-                    <i class="fas fa-heart"> 600</i>
-                    <i class="fas fa-minus-square"> 121</i>
-                </div>
-            </div>
-        </div>
-    </section>
+        <form action="change_avatar" method="post" enctype="multipart/form-data" id="avatarForm">
+            <img class="avatar_preview" src="../../public/assets/profile_empty.png" alt="avatar_preview">
+
+            <input type="file" id="avatarInput" name="avatar" accept="image/*">
+            <button type="submit">Upload Avatar</button>
+        </form>
+    </div>
+
+    </div>
 </body>
 </html>
